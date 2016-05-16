5162621f34793a3e1ca91b471577feb2e9cf8732
Pedro Villaroman <pedrovillaroman@gmail.com>
2016-05-15 17:56:25 -0700

    Connected JawsDB MySQL Database

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  DATABASE_HOST: '127.0.0.1',
-  DATABASE_USER: 'root',
-  DATABASE_PASSWORD: 'gerger123',
-  DATABASE_NAME: 'bookmarx',
-  PORT: 3600,
+  // DATABASE_HOST: '127.0.0.1',
+  // DATABASE_USER: 'root',
+  // DATABASE_PASSWORD: 'gerger123',
+  // DATABASE_NAME: 'bookmarx',
+  DATABASE_HOST: 'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  DATABASE_USER: 'kak7ysodj7sx4lvn',
+  DATABASE_PASSWORD: 'kacu3njlcyjufvb9',
+  DATABASE_NAME: 'g68y5311ia4r39qy',
+  PORT: 3306,
   USERNAME: 'cse136User',
   PASSWORD: 'cse136PW'
 };
